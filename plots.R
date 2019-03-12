59eb6e4bb9df5fbacdecdd7d46488be04c2b13c8
Sean Cleary <clear1@uw.edu>
2019-03-11 18:40:20 -0700

    plot 5 complete

--- plots.R
+++ plots.R
@@ -1,21 +1,21 @@
 library(dplyr)
 library(ggplot2)
+library(scales)
 source("analysis.r")
 
 crime_rain_data <- read.csv("trimmed_data.csv", stringsAsFactors = FALSE)
-#View(my_data)
 
 
 rainy_crimes <- crime_rain_data %>%
   filter(rain == TRUE) %>%
   count(subcategory)%>%
   arrange(desc(n))%>%
-  top_n(10, n)
+  top_n(9, n)
 
 plot1 <- ggplot(data = rainy_crimes) +
   geom_col(
     mapping = aes(x = reorder(subcategory, n), y = n, fill = subcategory)) +
-  scale_color_brewer(palette = "Spectral") +
+  scale_fill_brewer(palette = "Set1") +
   theme(axis.text.x = element_blank(),
         axis.ticks = element_blank())+
   labs(
@@ -31,7 +31,7 @@ second_plot_data <- crime_rain_data %>%
  
   
 
-View(second_plot_data)
+#View(second_plot_data)
 
 plot_two <- ggplot(data = second_plot_data) +
   geom_point(
@@ -51,7 +51,7 @@ third_plot_data <- crime_rain_data %>%
   filter(neighborhood == "CAPITOL HILL") %>%
 count(subcategory, PRCP, neighborhood)
 
-View(third_plot_data)
+#View(third_plot_data)
 
 
 plot_3 <- ggplot(data = third_plot_data) + 
@@ -72,7 +72,7 @@ plot_4_data <- crime_rain_data %>%
   count(subcategory, neighborhood, TMAX)
 
 
-View(plot_4_data)
+#View(plot_4_data)
 
 plot_4 <- ggplot(data = plot_4_data) + 
   geom_hex(mapping = aes(x = n, y = TMAX))+
@@ -84,12 +84,33 @@ plot_4 <- ggplot(data = plot_4_data) +
 
 plot_4
 
-
-
 #Table for Seasonal Plot
-my_data$MonthN <- as.numeric(format(as.Date(my_data$date),"%m")) # Month's number
-my_data$YearN <- as.numeric(format(as.Date(my_data$date),"%Y"))
-my_data$Month  <- months(as.Date(my_data$date), abbreviate=TRUE) # Month's abbr.
-View(my_data)
+crime_rain_data$MonthN <- as.numeric(format(as.Date(crime_rain_data$date),"%m")) # Month's number
+
+crime_numbers <- crime_rain_data %>% 
+  filter((rain == FALSE)&neighborhood == "UNIVERSITY") %>% 
+  count(MonthN, subcategory)
+
+mean_crimes <- crime_numbers %>% 
+  group_by(subcategory) %>% 
+  summarize(m = mean(n)) %>% 
+  top_n(9, m)
+
+crime_numbers <- crime_numbers %>% 
+  inner_join(mean_crimes, by = "subcategory")
+
+View(crime_numbers)
 
 
+
+plot_5 <- ggplot(data = crime_numbers, mapping = aes(x = MonthN, y = n, color = subcategory, size = "3")) +
+  geom_line() + 
+  geom_point() + 
+  guides(size = FALSE) +
+  scale_x_continuous(breaks = seq(1,12,1),
+                     labels = c("Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec")) +
+  labs(title = paste0("Seasonal Crimes in ", "XXX"), x = "Month", y="Number of Crimes") +
+  scale_color_brewer(palette = "Set1")
+  
+plot_5
+  
